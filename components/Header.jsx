8f815390e54687f09ffe156f0a7f7ea14fb06197
Rudeus <prathamkamthan1306@gmail.com>
2025-01-03 22:52:20 +0530

    feat: changed the products drop down and added images in it

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,13 @@
-'use client'
 import Image from 'next/image'
 import React from 'react'
 import logo from '@/app/assets/logo.svg'
 import Link from 'next/link'
 import ChevronRight from '@/app/assets/chevron-right.svg'
-import { useRouter } from 'next/navigation'
+import ZiniTransparent from '@/app/assets/zini-transparent.svg'
+import ZenoTransparent from '@/app/assets/zeno-transparent.svg'
+import ZoroTransparent from '@/app/assets/zoro-transparent.svg'
 
 const Header = () => {
-    const router = useRouter()
-    function handleProductClick(){
-        router.push('/products')
-    }
   return (
     <nav className='flex relative justify-between px-4 h-28 items-center' suppressHydrationWarning={true}>
         <Link href={'/'}>
@@ -20,43 +17,21 @@ const Header = () => {
             <Link className='flex justify-center items-center' href={'/'}>
                 <button>Home</button>
             </Link>
-            <div className='flex group hover:h-[105px] justify-center items-center'>
-                <button onClick={handleProductClick}>Products</button>
+            <div className='flex group hover:h-[130px] justify-center items-center'>
+                <button>Products</button>
                 <div className='absolute hidden group-hover:block top-full z-50 h-[350px] '>
                     <div className='w-full relative h-full flex'>
-                    <div className='h-full w-[340px] flex flex-col items-center text-white'>
-                    <Link className='w-full flex peer justify-between px-8 bg-[#49286E] border-b border-[#959393] py-2' href={'/products/zini'} >
+                    <div className='h-full w-[200px] overflow-hidden flex flex-col items-center text-white'>
+                    <Link className='w-full rounded-t-3xl hover:bg-[#3f225f] flex parent justify-between px-8 bg-[#49286E] border-b border-[#959393] py-2' href={'/products/zini'} >
                         <div>Zini</div>
                         <Image src={ChevronRight}  alt='Chevron Right'  width={25} height={25} />
-                    </Link>
-                    <Link className='w-full px-8 peer flex justify-between bg-[#49286E] border-b border-[#959393] py-2' href={'/products/zeno'} >
-                        <div>Zeno</div>
-                        <Image src={ChevronRight}  alt='Chevron Right'  width={25} height={25} />
-                    </Link>
-                    <Link className='w-full peer px-8 flex justify-between bg-[#49286E] border-b border-[#959393] py-2' href={'/products/zoro'} >
-                        <div>Zoro</div>
-                        <Image src={ChevronRight}  alt='Chevron Right'  width={25} height={25} />
-                    </Link>
-                    <div className='w-[340px] bg-white text-black -right-full absolute peer-hover:block hidden min-h-full space-y-16 shadow-md rounded-xl py-4 border mx-auto px-8'>
+                        <div className='w-[424px] h-[227px] child bg-white text-black -right-[212%] absolute hidden shadow-md rounded-xl border mx-auto px-8'>
                         {/* <div className='space-y-16 shadow-md rounded-xl py-4 border w-11/12 h-11/12 mx-auto mt-1 px-8'> */}
+                        <div className='flex justify-around  items-center'>
                         <h1 className='font-semibold text-2xl'>ZINI</h1>
-                        <div className='grid grid-rows-2 grid-cols-2 gap-5'>
-                            <div>
-                                <h3 className='text-xs font-semibold'>ABC</h3>
-                                <p className='text-[8px] font-normal'>Orbit is your portal for real-time fleet 
-management and analysis. Eliminate 
-guesswork, make informed decisions, 
-and increase efficiency throughout your facility.
-</p>
-                            </div>
-                            <div>
-                                <h3 className='text-xs font-semibold'>ABC</h3>
-                                <p className='text-[8px] font-normal'>Orbit is your portal for real-time fleet 
-management and analysis. Eliminate 
-guesswork, make informed decisions, 
-and increase efficiency throughout your facility.
-</p>
-                            </div>
+                        <Image src={ZiniTransparent} alt='Zini' width={90} height={90}   />
+                        </div>
+                        <div className='grid grid-rows-1 grid-cols-2 gap-5'>
                             <div>
                                 <h3 className='text-xs font-semibold'>ABC</h3>
                                 <p className='text-[8px] font-normal'>Orbit is your portal for real-time fleet 
@@ -77,6 +52,69 @@ and increase efficiency throughout your facility.
                         {/* </div> */}
 
                     </div>
+                    </Link>
+                    <Link className='w-full px-8 parent hover:bg-[#3f225f] flex justify-between bg-[#49286E] border-b border-[#959393] py-2' href={'/products/zeno'} >
+                        <div>Zeno</div>
+                        <Image src={ChevronRight}  alt='Chevron Right'  width={25} height={25} />
+                        <div className='w-[424px] h-[227px] child bg-white text-black -right-[212%] absolute hidden shadow-md rounded-xl border mx-auto px-8'>
+                        {/* <div className='space-y-16 shadow-md rounded-xl py-4 border w-11/12 h-11/12 mx-auto mt-1 px-8'> */}
+                        <div className='flex justify-around  items-center'>
+                        <h1 className='font-semibold text-2xl'>ZENO</h1>
+                        <Image src={ZenoTransparent} alt='Zini' width={90} height={90}   />
+                        </div>
+                        <div className='grid grid-rows-1 grid-cols-2 gap-5'>
+                            <div>
+                                <h3 className='text-xs font-semibold'>ABC</h3>
+                                <p className='text-[8px] font-normal'>Orbit is your portal for real-time fleet 
+management and analysis. Eliminate 
+guesswork, make informed decisions, 
+and increase efficiency throughout your facility.
+</p>
+                            </div>
+                            <div>
+                                <h3 className='text-xs font-semibold'>ABC</h3>
+                                <p className='text-[8px] font-normal'>Orbit is your portal for real-time fleet 
+management and analysis. Eliminate 
+guesswork, make informed decisions, 
+and increase efficiency throughout your facility.
+</p>
+                            </div>
+                        </div>
+                        {/* </div> */}
+
+                    </div>
+                    </Link>
+                    <Link className='w-full parent px-8 rounded-b-3xl hover:bg-[#3f225f] flex justify-between bg-[#49286E] border-b border-[#959393] py-2' href={'/products/zoro'} >
+                        <div>Zoro</div>
+                        <Image src={ChevronRight}  alt='Chevron Right'  width={25} height={25} />
+                        <div className='w-[424px] h-fit pb-8 child bg-white text-black -right-[212%] absolute hidden shadow-md rounded-xl border mx-auto px-8'>
+                        {/* <div className='space-y-16 shadow-md rounded-xl py-4 border w-11/12 h-11/12 mx-auto mt-1 px-8'> */}
+                        <div className='flex justify-around  items-center'>
+                        <h1 className='font-semibold text-2xl'>ZORO</h1>
+                        <Image src={ZoroTransparent} alt='Zini' width={90} height={90}   />
+                        </div>
+                        <div className='grid grid-rows-1 grid-cols-2 gap-5'>
+                            <div>
+                                <h3 className='text-xs font-semibold'>ABC</h3>
+                                <p className='text-[8px] font-normal'>Orbit is your portal for real-time fleet 
+management and analysis. Eliminate 
+guesswork, make informed decisions, 
+and increase efficiency throughout your facility.
+</p>
+                            </div>
+                            <div>
+                                <h3 className='text-xs font-semibold'>ABC</h3>
+                                <p className='text-[8px] font-normal'>Orbit is your portal for real-time fleet 
+management and analysis. Eliminate 
+guesswork, make informed decisions, 
+and increase efficiency throughout your facility.
+</p>
+                            </div>
+                        </div>
+                        {/* </div> */}
+
+                    </div>
+                    </Link>
                     </div>
                     </div>
                 </div>
